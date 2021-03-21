820518dfffd573365726a624d2272b03e107c827
binay shah <binayshah@binays-MacBook-Pro.local>
2021-03-21 08:41:36 +0545

    modified

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.todoapp.data.AppDatabase;
+import com.example.todoapp.data.Repository;
 import com.example.todoapp.data.Task;
 import com.example.todoapp.data.TodoDao;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -16,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView recyclerView;
+    private TaskAdapter adapter;
+    private Repository repository;
+    private FloatingActionButton addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +30,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.task_list);
+
+        repository = Repository.getRepository(this.getApplication());
+        List<Task> tasks = repository.getALlTasks();
+        adapter = new TaskAdapter(tasks);
+        recyclerView.setAdapter(adapter);
+
+        addButton = findViewById(R.id.add_btn);
+        addButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/todoapp/TaskAdapter.java
+++ app/src/main/java/com/example/todoapp/TaskAdapter.java
@@ -1,34 +1,64 @@
 package com.example.todoapp;
 
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class TaskAdapter extends RecyclerView.Adapter<> {
+import com.example.todoapp.data.Task;
 
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
+
+    private List<Task> data;
+    public TaskAdapter(List<Task> tasks) {
+        data = tasks;
+    }
+
+    public void setData(List<Task> tasks){
+        data = tasks;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public TaskAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        //View view = ;
+        return new ViewHolder(inflater,parent);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull TaskAdapter.ViewHolder holder, int position) {
+        Task task = data.get(position);
+        holder.bind(task);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if(data == null)
+            return  0;
+        return data.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+
+        private TextView title;
+        private TextView description;
+        public ViewHolder(LayoutInflater inflater, @NonNull ViewGroup parent) {
+            super(inflater.inflate(R.layout.task_item, parent, false));
+            title = itemView.findViewById(R.id.title_tv);
+            description = itemView.findViewById(R.id.description_tv);
         }
+
+        public void bind(Task task) {
+            title.setText(task.getTitle());
+            description.setText(task.getDescription());
+        }
+
+
     }
 }
